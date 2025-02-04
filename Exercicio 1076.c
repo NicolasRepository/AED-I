3de3c10f31047adccfa06e2d4edf20c1929996f5
Nicolas Almeida Faria <162118961+NicolasRepository@users.noreply.github.com>
2025-02-03 21:38:48 -0300

    Create Exercicio 1076.c

---
+++ Exercicio 1076.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n, m, lixo, arestas, arestasT, x, y;
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        scanf("%d", &lixo);
+        scanf("%d %d", &m, &arestas);
+        arestasT = arestas;
+        int *grafo = (int *)calloc(m * m, sizeof(int));
+        for(int j = 0; j < arestas; j++){
+            scanf("%d %d", &x, &y);
+            if(grafo[x * m + y] % 2 == 1){
+                arestasT--;
+            }
+            else if(grafo[x * m + y] > 0){
+                arestasT++;
+            }
+            else{
+                grafo[x * m + y] = grafo[y * m + x] = 1;
+            }
+        }
+        free(grafo);
+        printf("%d\n", arestasT*2);
+    }
+    return 0;
+}
